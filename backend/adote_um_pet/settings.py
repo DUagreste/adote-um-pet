fc84f061de9331afc405ba93dfb811185ca9eb26
Vítor Pinheiro <vitor.pinheiro97@hotmail.com>
2022-06-07 17:31:14 -0300

    upgrade: add serviço de envio de email para confirmação de adoção.

--- backend/adote_um_pet/settings.py
+++ backend/adote_um_pet/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+from django.db.models.fields import EmailField
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -130,3 +132,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 APPEND_SLASH = False
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+# EMAIL
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "adote.pet.rn@gmail.com"
+EMAIL_HOST_PASSWORD = "xnewwlpxgakqcafu"
