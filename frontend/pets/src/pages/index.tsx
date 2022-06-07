5b9d812adc04b94809ef75537db49f81bbf33032
Vítor Pinheiro <vitor.pinheiro97@hotmail.com>
2022-06-06 23:52:56 -0300

    update: add title and subtitle.

--- frontend/pets/src/pages/index.tsx
+++ frontend/pets/src/pages/index.tsx
@@ -1,10 +1,18 @@
 import type { NextPage } from 'next'
+import Title from '../ui/components/title/title'
 
 
 const Home: NextPage = () => {
   return (
     <div>
-      
+      <Title 
+        title="" 
+        subtitle={
+          <span>
+            Com um pequeno valor mensal, você <br />
+            pode <strong>adotar um pet virtual.</strong>
+          </span>
+        }/>
     </div>
   )
 }
