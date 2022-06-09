67daf1d626883d1c50cc5cb5183d23af17185bfb
Vítor Pinheiro <vitor.pinheiro97@hotmail.com>
2022-06-09 12:10:30 -0300

    up: adicionado a header admin.

--- frontend/pets/src/pages/_app.tsx
+++ frontend/pets/src/pages/_app.tsx
@@ -3,11 +3,15 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@mui/material'
 import theme from '../ui/themes/theme'
 import Header from '../ui/components/header/header'
+import HeaderAdmin from '../ui/components/header-admin/header-admin'
+import { useRouter } from 'next/router'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   return (
     <ThemeProvider theme={theme}>
-      <Header/>
+      {router.pathname === '/' ? <Header/> : <HeaderAdmin/>}
       <Component {...pageProps} />
     </ThemeProvider>
   )
